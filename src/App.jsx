0f21fcb31d370ab08c93102a43576d664d085c4e
zelttikDevv <alberrttoop@gmail.com>
2026-07-02 13:31:36 -0500

    Create App.jsx

---
+++ src/App.jsx
@@ -0,0 +1,37 @@
+import { BrowserRouter, Routes, Route, Navigate } from "react-router-dom";
+import { AuthProvider } from "./contexts/AuthContext";
+import ProtectedRoute from "./components/ProtectedRoute";
+import Login from "./pages/Login";
+import AdminDashboard from "./pages/AdminDashboard";
+import ReceptionistDashboard from "./pages/ReceptionistDashboard";
+
+function App() {
+  return (
+    <AuthProvider>
+      <BrowserRouter>
+        <Routes>
+          <Route path="/login" element={<Login />} />
+          <Route
+            path="/admin"
+            element={
+              <ProtectedRoute allowedRoles={["admin"]}>
+                <AdminDashboard />
+              </ProtectedRoute>
+            }
+          />
+          <Route
+            path="/recepcion"
+            element={
+              <ProtectedRoute allowedRoles={["recepcionista", "admin"]}>
+                <ReceptionistDashboard />
+              </ProtectedRoute>
+            }
+          />
+          <Route path="/" element={<Navigate to="/login" replace />} />
+        </Routes>
+      </BrowserRouter>
+    </AuthProvider>
+  );
+}
+
+export default App;
